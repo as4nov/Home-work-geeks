2a6b5385a824d966a120949bce5907d23ff76a8c
as4nov <133018273+as4nov@users.noreply.github.com>
2023-06-17 14:56:59 +0600

    выполнил DZ3

---
+++ Project-3-month_Ulanbek/Project-3-month-new-main/js/lesson.js
@@ -0,0 +1,96 @@
+// REG EXP
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
+
+const regExp = /^\+996 \d{3} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'YOUR NUMBER IS VALID!'
+        phoneResult.style.color = 'green'
+    } else {
+        phoneResult.innerHTML = 'YOUR NUMBER IS NOT VALID'
+        phoneResult.style.color = 'red'
+    }
+}
+
+// TAB SLIDER
+
+const tabContent = document.querySelectorAll('.tab_content_block')
+const tabsParent = document.querySelector('.tab_content_items')
+const tabs = document.querySelectorAll('.tab_content_item')
+
+const hideTabContent = () => {
+    tabContent.forEach((item) => {
+        item.style.display = 'none'
+    })
+    tabs.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (i = 0) => {
+    tabContent[i].style.display = 'block'
+    tabs[i].classList.add('tab_content_item_active')
+}
+
+let currentIndex = 0
+let intervalId
+
+const switchTab = () => {
+    hideTabContent();
+    currentIndex = currentIndex + 1
+    if (currentIndex === tabContent.length) {
+        currentIndex = 0
+    }
+    showTabContent(currentIndex)
+}
+
+intervalId = setInterval(switchTab, 3000)
+
+hideTabContent()
+showTabContent()
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabs.forEach((item, i) => {
+            if (event.target === item) {
+                hideTabContent()
+                showTabContent(i)
+            }
+        })
+    }
+}
+
+// MODAL
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const closeModalButton = document.querySelector('.modal_close')
+
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+
+}
+
+const openEndScroll = () => {
+    const scrollPosition = document.documentElement.scrollTop
+    const windowHeight = window.innerHeight
+    const fullHeight = document.documentElement.scrollHeight
+
+    if (scrollPosition + windowHeight >= fullHeight) {
+        openModal()
+    }
+}
+
+window.addEventListener('scroll', openEndScroll)
+setTimeout(openModal, 10000)
+modalTrigger.onclick = () => openModal()
+closeModalButton.onclick = () => closeModal()
+modal.onclick = (event) => event.target === modal && closeModal()
